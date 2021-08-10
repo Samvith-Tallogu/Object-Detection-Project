ef48825bf6e4d7230c4ad3e423e14622dad8644e
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-08-10 15:45:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function room() {
+    window.location = "room.html";
+}
+function tvac() {
+    window.location = "tvac.html";
+}
+function desk() {
+    window.location = "desk.html";
+}
+function bottles() {
+    window.location = "bottles.html";
+}
+function fruits() {
+    window.location = "fruitbasket.html";
+}
+
+function back() {
+    window.location = "index.html";
+}
